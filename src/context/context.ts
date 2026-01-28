22cadd2213690ebe12cfb6561ece8d3b15f33455
Rik Brown <rik@rik.codes>
2026-01-28 17:30:51 +0000

    wip

--- src/context/context.ts
+++ src/context/context.ts
@@ -87,6 +87,9 @@ export class Context {
     if (isString(paths)) paths = paths.split('.')
     for (let i = 0; i < paths.length; i++) {
       scope = yield this.readProperty(scope as object, paths[i])
+      if (scope instanceof Error) {
+        throw scope
+      }
       if (strictVariables && isUndefined(scope)) {
         throw new InternalUndefinedVariableError((paths as string[]).slice(0, i + 1).join!('.'))
       }
@@ -121,7 +124,7 @@ export class Context {
     return this.globals
   }
   * readProperty (obj: Scope, key: (PropertyKey | Drop)) {
-    obj = yield toLiquid(obj)
+    obj = toLiquid(obj)
     key = toValue(key) as PropertyKey
     if (isNil(obj)) return obj
     if (isArray(obj) && (key as number) < 0) return obj[obj.length + +key]
